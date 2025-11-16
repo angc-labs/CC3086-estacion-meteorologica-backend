af8ae9af9b57fd94c7b915235ab539e4a0c5549e
angc <angcoder.c@gmail.com>
2025-11-16 14:45:59 -0600

    endpoint to insert readings in supabase, replace edge function

--- app/api/readings/route.ts
+++ app/api/readings/route.ts
@@ -6,6 +6,56 @@ const supabase = createClient(
   process.env.SUPABASE_ANON_KEY!
 );
 
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const {
+        ppm_alcohol,
+        ppm_h2,
+        ppm_co,
+        ppm_propano,
+        ppm_ch4,
+        lux,
+        temp_bmp,
+        pres,
+        alt,
+        hum,
+        temp_dht,
+        lluvia,
+        open
+    } = body;
+
+
+    const { error } = await supabase.from("readings").insert({
+        ppm_alcohol,
+        ppm_h2,
+        ppm_co,
+        ppm_propano,
+        ppm_ch4,
+        lux,
+        temp_bmp,
+        pres,
+        alt,
+        hum,
+        temp_dht,
+        lluvia,
+        open
+    });
+
+    if (error) {
+      console.error(error);
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+
+    return NextResponse.json({ ok: true });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
+
+
 export async function GET() {
   const { data, error } = await supabase
     .from("readings")
