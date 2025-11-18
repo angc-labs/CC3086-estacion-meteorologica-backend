a79bbbfa99072799f2c529bd183342ff8657f014
Tiffany24630 <sal24630@uvg.edu.gt>
2025-11-17 22:53:50 -0600

    Cambios

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: [],
-  },
+  // Configuración básica - sin opciones experimentales obsoletas
 }
 
 module.exports = nextConfig
